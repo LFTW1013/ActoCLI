ac89787c503036dcaedcf3e899cedf462259b60c
LFTW1013 <jacksonfam.1006@gmail.com>
2025-10-25 19:12:18 -0500

    Add files via upload

---
+++ ActoCLI/commands/help.js
@@ -0,0 +1,11 @@
+// help.js
+
+function help(printLine) {
+    printLine("Available commands:");
+    printLine("help - Shows this help message");
+    printLine("clr - Clears the terminal");
+    printLine("exit - Exits the CLI");
+    //any command added goes here
+}
+
+module.exports = help;
